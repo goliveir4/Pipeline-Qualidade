76588812ca158b06a4458a57fb1c3a0736a7899e
goliveir4 <113403360+goliveir4@users.noreply.github.com>
2025-06-06 17:17:46 -0300

    Create calculator_test.dart

---
+++ test/calculator_test.dart
@@ -0,0 +1,36 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import '../lib/calculator.dart'; 
+
+void main() {
+  group('Calculator Tests', () {
+    Calculator calculator;
+
+    setUp(() {
+      calculator = Calculator();
+    });
+
+    test('A função add deve retornar a soma correta de dois números', () {
+      expect(calculator.add(2, 3), 5);
+    });
+
+    test('A função add deve funcionar com números negativos', () {
+      expect(calculator.add(-1, -5), -6);
+      expect(calculator.add(-1, 5), 4);
+    });
+
+    test('A função add deve funcionar com zero', () {
+      expect(calculator.add(0, 0), 0);
+      expect(calculator.add(5, 0), 5);
+    });
+
+    test('A função subtract deve retornar a subtração correta', () {
+      expect(calculator.subtract(5, 3), 2);
+    });
+
+    test('A função subtract deve funcionar com números negativos', () {
+      expect(calculator.subtract(-1, -5), 4);
+      expect(calculator.subtract(1, -5), 6);
+    });
+  });
+}
